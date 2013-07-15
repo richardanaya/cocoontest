e618ef08adbacf9a99d866a4689fbd06c3c2cbc6
Richard Anaya <ranaya@evernote.com>
2013-07-15 14:07:28 -0500

    t

--- pointer.js
+++ pointer.js
@@ -184,8 +184,23 @@
          */
         trigger: function triggerEvent(gesture, eventData){
             // create DOM event
-            var event = Hammer.DOCUMENT.createEvent('Event');
-            event.initEvent(gesture, true, true);
+            var event = {
+                bubbles: true,
+                cancelBubble: false,
+                cancelable: true,
+                clipboardData: undefined,
+                currentTarget: null,
+                defaultPrevented: false,
+                eventPhase: 0,
+                gesture: Object,
+                returnValue: true,
+                srcElement: null,
+                target: null,
+                timeStamp: Date.now(),
+                type: gesture
+            };
+            //Hammer.DOCUMENT.createEvent('Event');
+            //event.initEvent(gesture, true, true);
             event.gesture = eventData;
 
             // trigger on the target if it is in the instance element,
